6f7aa0ecbea47d778a99ba2de564d0c5d518e8cb
Ася Тамонова <asyatamoniva@MacBook-Air-Asa.local>
2022-09-12 15:24:45 +0300

    Validation

---
+++ src/test/java/ru/netology/ValidationTest.java
@@ -0,0 +1,51 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValidationTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void ShouldFailNameV1AllEmpty() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.className("button__text")).click();
+
+        List<WebElement> elements = driver.findElements(By.className("input__sub"));
+        String text = elements.get(0).getText();
+
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+}
