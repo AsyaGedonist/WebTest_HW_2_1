2b030850339387462bfef39c3e66ca3d72cfda71
Ася Тамонова <asyatamoniva@MacBook-Air-Asa.local>
2022-09-12 14:55:01 +0300

    CSS search

--- src/test/java/AppOrderTestV1.java
+++ src/test/java/AppOrderTestV1.java
@@ -50,4 +50,15 @@ public class AppOrderTestV1 {
         assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
 
+    @Test
+    void ShouldTestSomethingV2 () {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79301111111");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
 }
