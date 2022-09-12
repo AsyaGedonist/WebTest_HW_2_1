f050157a0a5f931d24aeabb010c35006077a9ecd
Ася Тамонова <asyatamoniva@MacBook-Air-Asa.local>
2022-09-12 14:06:59 +0300

    Test ok

--- src/test/java/AppOrderTestV1.java
+++ src/test/java/AppOrderTestV1.java
@@ -3,9 +3,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppOrderTestV1 {
 
@@ -33,7 +39,15 @@ public class AppOrderTestV1 {
 
     @Test
     void ShouldTestSomething () {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Андрей");
+        elements.get(1).sendKeys("+79301111111");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
 
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
 
 }
